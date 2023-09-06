b563fdca44d22fc7bf2bb09c0cad5365043360c5
LYT-04 <why1605044412@163.com>
2023-09-06 15:14:48 +0800

    Initial commit

---
+++ src/main/java/top/yhw/javawebstudy/HelloApplication.java
@@ -0,0 +1,9 @@
+package top.yhw.javawebstudy;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}
---
+++ src/main/java/top/yhw/javawebstudy/HelloServlet.java
@@ -0,0 +1,6 @@
+package top.yhw.javawebstudy;
+
+import jakarta.servlet.http.HttpServlet;
+
+public class HelloServlet extends HttpServlet {
+}
