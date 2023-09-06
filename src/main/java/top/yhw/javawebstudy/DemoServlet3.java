33b419b88360dd77d1fa965e3534b566d7010da7
LYT-04 <why1605044412@163.com>
2023-09-06 15:36:13 +0800

    demo返回验证码，demo2返回网页，demo3返回json

---
+++ src/main/java/top/yhw/javawebstudy/DemoServlet3.java
@@ -0,0 +1,39 @@
+package top.yhw.javawebstudy;
+
+import jakarta.servlet.ServletConfig;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/demo3")
+public class DemoServlet3 extends HttpServlet {
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        // 返回一段json
+        resp.setContentType("application/json;charset=utf-8");
+        PrintWriter out = resp.getWriter();
+        String jsonString ="{\"name\":\"why\",\"age\":20}";
+//        String jsonString = """
+//                {
+//                "name":"why",
+//                "age":"20"
+//                }
+//                """;
+        out.print(jsonString);
+        out.flush();
+        out.close();
+    }
+
+    @Override
+    public void destroy() {
+    }
+}
